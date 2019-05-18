b2e6fc4775cc5eafc4b3ea6fcf6e3abd5bc4d59e
VacaRent <sales@vacarent.com>
2019-05-18 03:18:30 +0300

    init repo

---
+++ search.php
@@ -0,0 +1,218 @@
+<!DOCTYPE html>
+<html xml:lang="es-es" lang="es-es">
+<head>
+    <title>| Mármoles de Bayas</title>
+    <meta charset="utf-8">
+    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+    <meta name="language" content="es-ES" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+  <meta name="format-detection" content="telephone=no"/>
+  <link rel="icon" href="images/favicon.ico" type="image/x-icon">
+
+  <!-- Bootstrap -->
+  <link href="css/bootstrap.css" rel="stylesheet">
+
+  <!-- Links -->
+  <link rel="stylesheet" href="css/style.css">
+  <link rel="stylesheet" href="css/search.css">
+
+  <!--JS-->
+  <script src="js/jquery.js"></script>
+  <script src="js/jquery-migrate-1.2.1.min.js"></script>
+
+
+  <!--[if lt IE 9]>
+  <div style=' clear: both; text-align:center; position: relative;'>
+    <a href="http://windows.microsoft.com/en-US/internet-explorer/..">
+      <img src="images/ie8-panel/warning_bar_0000_us.jpg" border="0" height="42" width="820"
+           alt="You are using an outdated browser. For a faster, safer browsing experience, upgrade for free today."/>
+    </a>
+  </div>
+  <script src="js/html5shiv.js"></script>
+  <![endif]-->
+  <script src='js/device.min.js'></script>
+</head>
+<body>
+<div class="page">
+  <!--========================================================
+                            HEADER
+  =========================================================-->
+  <header>
+    <section class="contact-section">
+      <div class="container">
+        <ul class="inline-list">
+          <li><a class="fa-tumblr-square" href="#"></a></li>
+          <li><a class="fa-twitter-square" href="#"></a></li>
+          <li><a class="fa-youtube" href="#"></a></li>
+          <li><a class="fa-rss" href="#"></a></li>
+        </ul>
+        <div class="contacts">
+          <address><span class="fa fa-map-marker"></span>POL. IND. BAYAS PAR 30. Miranda de Ebro BURGOS (09200)
+          </address>
+          <a href="mailto:contacto@marmolesdebayas.com"><span class="fa fa-envelope"></span>contacto@marmolesdebayas.com</a>
+          <a href="callto:947333141"><span class="fa fa-phone"></span>947 333 141</a>
+        </div>
+      </div>
+    </section>
+
+    <div id="stuck_container" class="stuck_container stuck_container__mod">
+      <nav class="navbar navbar__mod navbar-default navbar-static-top ">
+        <div class="container">
+          <div class="navbar-header">
+            <h1 class="navbar-brand">
+              <a data-type='rd-navbar-brand' href="./"><span>Marmoles </span>&nbsp;de Bayas
+              </a>
+            </h1>
+          </div>
+
+          <ul class="navbar-nav sf-menu navbar-right" data-type="navbar">
+            <li>
+              <a href="./">Home</a>
+            </li>
+            <li class="dropdown">
+              <a href="Marmoles.html">About</a>
+              <ul class="dropdown-menu">
+                <li>
+                  <a href="#">History</a>
+                </li>
+                <li>
+                  <a href="#">News</a>
+                  <ul class="dropdown-menu">
+                    <li>
+                      <a href="#">Latest</a>
+                    </li>
+                    <li>
+                      <a href="#">Archive</a>
+                    </li>
+                  </ul>
+                </li>
+                <li>
+                  <a href="#">Testimonials</a>
+                </li>
+              </ul>
+            </li>
+            <li>
+              <a href="ExposicionMarmoles.html">Programs</a>
+            </li>
+            <li>
+              <a href="index-3.html">Solutions</a>
+            </li>
+
+            <li>
+              <a href="GaleriaMarmoles.html">Current projects </a>
+            </li>
+            <li>
+              <a href="Contacto.html"> Contacto </a>
+            </li>
+          </ul>
+
+        </div>
+      </nav>
+    </div>
+
+  </header>
+
+
+  <!--========================================================
+                            CONTENT
+  =========================================================-->
+  <main>
+    <section id="content" class="content well">
+        <div class="container">
+           <form class="search-form left991" action="search.php" method="GET" accept-charset="utf-8">
+                                     <label class="search-form_label">
+                                         <input class="search-form_input" type="text" name="s" autocomplete="off"/>
+                                         <span class="search-form_liveout"></span>
+                                     </label>
+                                     <button class="search-form_submit btn btn-primary btn-sm" type="submit"><span>Search</span></button>
+                                 </form>
+            <h2>Search Results</h2>
+            <div id="search-results"></div>
+        </div>
+    </section>
+  </main>  
+
+  <!--========================================================
+                          FOOTER
+  =========================================================-->
+  <footer>
+
+        <section>
+            <div class="container center767">
+                <div class="row">
+                    <div class="col-md-4 col-sm-4 col-xs-12">
+                        
+
+                        <p>
+                            <a class="brand" title="Distribuidores de mármol en el País Vasco" href="Contacto.html">Mármoles de Bayas</a> <br/> <span id="copyright-year"></span>
+                            <a href="AvisoLegal.html">
+                                Aviso Legal
+                            </a>
+                            <!-- {%FOOTER_LINK} -->
+                        </p>
+
+                        <!--<ul class="inline-list">
+                            <li><a class="fa-tumblr-square" href="#"></a></li>
+                            <li><a class="fa-twitter-square" href="#"></a></li>
+                            <li><a class="fa-youtube" href="#"></a></li>
+                            <li><a class="fa-rss" href="#"></a></li>
+                        </ul>-->
+                    </div>
+
+
+                    <div class="col-md-4 col-sm-4 col-xs-12">
+                        <h5>
+                            Contacto
+                        </h5>
+
+                        <div class="contact-info">
+                            <address>POL. IND. BAYAS PAR 30<br/>Miranda de Ebro BURGOS (09200) </address>
+                            <dl>
+                                <dt>Teléfono:</dt>
+                                <dd>
+                                    <a href="callto:947 333 141"> 947 333 141</a>
+                                </dd>
+                            </dl>
+                            <dl>
+                                <dt>E-mail:</dt>
+                                <dd>
+                                    <a href="mailto:contacto@marmolesdebayas.com">contacto@marmolesdebayas.com</a>
+                                </dd>
+                            </dl>
+                        </div>
+                    </div>
+                    <div class="col-md-4 col-sm-4 col-xs-12">
+                        <h5>
+                            Servicios
+                        </h5>
+
+                        <div class="contact-info">
+                            <ul>
+                                <li><a title="Portales de mármol" href="EncimerasCocinas.html">Encimeras de cocina</a></li>
+                                <li><a title="Locales comerciales en mármol" href="LocalesComerciales.html">Locales comerciales</a></li>
+                                <li><a title="Comunidades de vecinos en mármol" href="ComunidadesVecinos.html">Comunidades de vecinos</a></li>
+                                <li><a title="Arte funerario en mármol" href="ArteFunerario.html">Arte funerario</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div style="text-align:right">
+                        <span style="font-size:9px;">Diseño Web <a href="http://culspot.com" target="_blank" style="color:orange" title="Diseño web y Posicionamiento SEO">www.culspot.com</a></span>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </footer>
+</div>
+
+
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<!-- Include all compiled plugins (below), or include individual files as needed -->
+<script src="js/bootstrap.min.js"></script>
+<script src="js/tm-scripts.js"></script>
+<!-- </script> -->
+
+
+</body>
+</html>
